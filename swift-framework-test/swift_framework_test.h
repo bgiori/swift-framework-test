615e50d7d3989f73a0a45d559a8e68f8431612ec
Brian Giori <brian.giori@amplitude.com>
2023-10-05 09:50:46 -0700

    Initial Commit

---
+++ swift-framework-test/swift_framework_test.h
@@ -0,0 +1,18 @@
+//
+//  swift_framework_test.h
+//  swift-framework-test
+//
+//  Created by Brian Giori on 10/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swift_framework_test.
+FOUNDATION_EXPORT double swift_framework_testVersionNumber;
+
+//! Project version string for swift_framework_test.
+FOUNDATION_EXPORT const unsigned char swift_framework_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_framework_test/PublicHeader.h>
+
+
